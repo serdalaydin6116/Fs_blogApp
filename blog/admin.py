988e4153263bf35973aefa0d300e8e57bb8694e7
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-10 23:06:04 +0300

    models were added to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import  Post, Comment, Like, PostView, Category
+
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(PostView)
+admin.site.register(Like)
